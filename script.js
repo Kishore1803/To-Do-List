b0342922c03a5d6e9e9171bab1024614d343fe67
Kishore1803 <kannan.v4704@gmail.com>
2025-12-03 21:44:22 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+// add a todo list
+function addTodo() {
+    let todoInput = document.getElementById('todoInput'); // get input box
+    let todoText = todoInput.value; // get input value
+     if (todoText === "") {  // prevent adding empty tasks
+        alert("Please enter a task!");
+        return;
+    }
+    let li = document.createElement("li");
+    li.textContent = todoText;
+    document.getElementById('todoList').appendChild(li);
+    todoInput.value = "";  // clear input box
+}
+
+// delete the todo list
+function deleteTodo() {
+    let todoList = document.getElementById('todoList');
+    if (todoList.lastChild) {
+        todoList.removeChild(todoList.lastChild);
+    }
+    else{
+        alert("No tasks to Delete!");
+    }
+}
